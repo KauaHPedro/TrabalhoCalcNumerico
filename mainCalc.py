b8bca995b5ce966846e1734f310844647bc1ee19
KauaHPedro <kauahcpedro@hotmail.com>
2024-09-20 21:45:56 -0300

    Finalizando implementação da Bisseção, utilizando recursividade

--- mainCalc.py
+++ mainCalc.py
@@ -1,28 +1,32 @@
-import math
-from xml.etree.ElementTree import tostring
+import numpy as np
 
-
-def calcularRaiz(funcao, a, b, erro):
-    if funcao(a) * funcao(b) >= 0:
-        print('intervalo inválido')
-
-    while (math.fabs(b -a)) / 2 > erro:
-        c = round((a + b) / 2, 6)
-        if round(funcao(c), 6) == 0 or round((b - a) / 2, 6) < erro:
-            return c
-        
-        if round(funcao(a), 6) * round(funcao(c), 6) < 0:
-            b = c
-        else:
-            a = c
+def calcularRaiz(f, a, b, erro): 
     
-    return c
+    if np.sign(f(a)) == np.sign(f(b)):
+        raise Exception(
+         "Intervalo inválido!")
+        
+    m = (a + b)/2
+    
+    if np.abs(f(m)) < erro:
+        # Para a condição, retorna M como a raíz da função
+        return m
+    
+    elif np.sign(f(a)) == np.sign(f(m)):
+        return calcularRaiz(f, m, b, erro)
+    
+    elif np.sign(f(b)) == np.sign(f(m)):
+        return calcularRaiz(f, a, m, erro)
 
 def f1(x):
-    return round(math.sqrt(5 - x) - 2 ** (x - 1), 6)
+    return round(np.sqrt(5 - x) - 2 ** (x - 1), 6)
 
 def f2(x):
-    return round(math.log(3 * x - 1) + 2 * x, 6)
+    return round(np.log(3 * x - 1) + 2 * x, 6)
 
-print(calcularRaiz(f1, 1, 2, 0.01))
-print(calcularRaiz(f2, 0.34, 0.5, 0.01))
+def f3(x):
+    return round(x + 5 * np.log(x) - 2)
+
+print(round(calcularRaiz(f1, 1, 2, 0.01), 6))
+print(round(calcularRaiz(f2, 0.34, 0.5, 0.01), 6))
+print(round(calcularRaiz(f3, 1, 2, 0.01), 6))
